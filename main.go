be6f73a5a47454bb8e199c14be6138536c32d8db
Ahmed Shalaby <kingshalaby@yahoo.com>
2021-02-09 20:50:04 +0200

    add quiz timeout

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"time"
 )
 
 const fileName = "problems.csv"
+var result *int
 
 func main() {
+
+	result = new(int)
 	f, err := os.Open(fileName)
 	if err != nil {
 		fmt.Printf("failed to open file: %v\n", err)
@@ -23,21 +27,36 @@ func main() {
 		return
 	}
 
-	var answer string
-	var result int
+	finished := startQuiz(questions)
+	timeUp := time.Tick(12 * time.Second)
 
-	for i, quest := range questions {
-		q, ans := quest[0], quest[1]
-		fmt.Printf("\n%d. %s = ", i+1, q)
-		if _, err := fmt.Scan(&answer); err != nil {
-			fmt.Printf("failed to scan %v", err)
-
-		}
-
-		if answer == ans {
-			result++
-		}
+	select{
+	case <-finished:
+	case <-timeUp:
 	}
 
-	fmt.Printf("your score: %d/%d\n", result, len(questions))
+	fmt.Printf("your score: %d/%d\n", *result, len(questions))
+}
+
+func startQuiz(questions [][]string) chan bool{
+	var answer string
+
+	finished := make(chan bool)
+
+	go func() {
+		for i, quest := range questions {
+			q, ans := quest[0], quest[1]
+			fmt.Printf("\n%d. %s = ", i+1, q)
+			if _, err := fmt.Scan(&answer); err != nil {
+				fmt.Printf("failed to scan %v", err)
+
+			}
+
+			if answer == ans {
+				*result++
+			}
+		}
+		finished <- true
+	}()
+	return finished
 }
